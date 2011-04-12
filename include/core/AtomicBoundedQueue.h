1cbaa30e40cc4a04fee5efe065d102f6524ff88f
Mu Chi Sung <mark@zillians.com>
2011-04-13 00:57:14 +0800

    Add atomic mpmc bounded queue and test cases

---
+++ include/core/AtomicBoundedQueue.h
@@ -0,0 +1,126 @@
+/**
+ * Zillians MMO
+ * Copyright (C) 2007-2010 Zillians.com, Inc.
+ * For more information see http://www.zillians.com
+ *
+ * Zillians MMO is the library and runtime for massive multiplayer online game
+ * development in utility computing model, which runs as a service for every
+ * developer to build their virtual world running on our GPU-assisted machines.
+ *
+ * This is a close source library intended to be used solely within Zillians.com
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * COPYRIGHT HOLDER(S) BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN
+ * AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+/**
+ * @date Apr 13, 2011 sdk - Initial version created.
+ */
+
+#ifndef ATOMICBOUNDEDQUEUE_H_
+#define ATOMICBOUNDEDQUEUE_H_
+
+#include "core/Common.h"
+#include "core/JustThread.h"
+
+namespace zillians {
+
+template<typename T>
+class AtomicBoundedQueue
+{
+public:
+	AtomicBoundedQueue(std::size_t buffer_size) : buffer(new cell_t[buffer_size]), buffer_mask(buffer_size - 1)
+	{
+		BOOST_ASSERT((buffer_size >= 2) && ((buffer_size & (buffer_size - 1)) == 0) && "the buffer size must be greater than 2 and is power of 2");
+        for (size_t i = 0; i != buffer_size; i += 1)
+            buffer[i].sequence.store(i, std::memory_order_relaxed);
+        enqueue_pos.store(0, std::memory_order_relaxed);
+        dequeue_pos.store(0, std::memory_order_relaxed);
+	}
+
+	~AtomicBoundedQueue()
+	{
+        delete [] buffer;
+	}
+
+public:
+    bool push(T const& data)
+    {
+        cell_t* cell;
+        size_t pos = enqueue_pos.load(std::memory_order_relaxed);
+        for (;;)
+        {
+            cell = &buffer[pos & buffer_mask];
+            size_t seq = cell->sequence.load(std::memory_order_acquire);
+            intptr_t dif = (intptr_t)seq - (intptr_t)pos;
+            if (dif == 0)
+            {
+                if (enqueue_pos.compare_exchange_weak(pos, pos + 1, std::memory_order_relaxed))
+                    break;
+            }
+            else if (dif < 0)
+                return false;
+            else
+                pos = enqueue_pos.load(std::memory_order_relaxed);
+        }
+
+        cell->data = data;
+        cell->sequence.store(pos + 1, std::memory_order_release);
+
+        return true;
+    }
+
+    bool pop(T& data)
+    {
+        cell_t* cell;
+        size_t pos = dequeue_pos.load(std::memory_order_relaxed);
+        for (;;)
+        {
+            cell = &buffer[pos & buffer_mask];
+            size_t seq = cell->sequence.load(std::memory_order_acquire);
+            intptr_t dif = (intptr_t)seq - (intptr_t)(pos + 1);
+            if (dif == 0)
+            {
+                if (dequeue_pos.compare_exchange_weak(pos, pos + 1, std::memory_order_relaxed))
+                    break;
+            }
+            else if (dif < 0)
+                return false;
+            else
+                pos = dequeue_pos.load(std::memory_order_relaxed);
+        }
+
+        data = cell->data;
+        cell->sequence.store(pos + buffer_mask + 1, std::memory_order_release);
+
+        return true;
+    }
+
+private:
+    struct cell_t
+    {
+        std::atomic<size_t>     sequence;
+        T                       data;
+    };
+
+    static size_t const         cacheline_size = 64;
+    typedef char                cacheline_pad_t[cacheline_size];
+
+    cacheline_pad_t             pad0;
+    cell_t* const               buffer;
+    size_t const                buffer_mask;
+    cacheline_pad_t             pad1;
+    std::atomic<size_t>         enqueue_pos;
+    cacheline_pad_t             pad2;
+    std::atomic<size_t>         dequeue_pos;
+    cacheline_pad_t             pad3;
+
+    AtomicBoundedQueue(AtomicBoundedQueue const&);
+    void operator = (AtomicBoundedQueue const&);
+};
+
+}
+#endif /* ATOMICBOUNDEDQUEUE_H_ */
---
+++ test/testzillians-common-threading/AtomicBoundedQueueTest/AtomicBoundedQueueTest.cpp
@@ -0,0 +1,227 @@
+/**
+ * Zillians MMO
+ * Copyright (C) 2007-2010 Zillians.com, Inc.
+ * For more information see http://www.zillians.com
+ *
+ * Zillians MMO is the library and runtime for massive multiplayer online game
+ * development in utility computing model, which runs as a service for every
+ * developer to build their virtual world running on our GPU-assisted machines.
+ *
+ * This is a close source library intended to be used solely within Zillians.com
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * COPYRIGHT HOLDER(S) BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN
+ * AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+/**
+ * @date Apr 13, 2011 sdk - Initial version created.
+ */
+
+#include "core/Prerequisite.h"
+#include "core/AtomicBoundedQueue.h"
+#include <iostream>
+#include <string>
+#include <limits>
+#include <tbb/tick_count.h>
+
+#define BOOST_TEST_MODULE AtomicBoundedQueueTest
+#define BOOST_TEST_MAIN
+#include <boost/test/unit_test.hpp>
+
+using namespace zillians;
+using namespace std;
+
+BOOST_AUTO_TEST_SUITE( AtomicBoundedQueueTest )
+
+BOOST_AUTO_TEST_CASE( AtomicBoundedQueueTestCase1 )
+{
+	AtomicBoundedQueue<int> queue(64);
+	for(int i=0;i<64;++i)
+	{
+		BOOST_CHECK(queue.push(i));
+	}
+	BOOST_CHECK(!queue.push(123));
+	for(int i=0;i<64;++i)
+	{
+		int x;
+		BOOST_CHECK(queue.pop(x));
+		BOOST_CHECK(x == i);
+	}
+}
+
+void producer_thread_proc(AtomicBoundedQueue<int>* q, int items_to_push)
+{
+    AtomicBoundedQueue<int>& queue = *q;
+
+    while(items_to_push > 0)
+    {
+    	if(queue.push(items_to_push))
+    	{
+//    		printf("pushed %d\n", items_to_push);
+    		--items_to_push;
+    	}
+    	else
+    		boost::this_thread::yield();
+    }
+
+}
+
+void consumer_thread_proc(AtomicBoundedQueue<int>* q, int items_to_pop)
+{
+    AtomicBoundedQueue<int>& queue = *q;
+
+    while(items_to_pop > 0)
+    {
+    	int x;
+    	if(queue.pop(x))
+    	{
+//    		printf("popped %d\n", x);
+    		--items_to_pop;
+    	}
+    	else
+    		boost::this_thread::yield();
+    }
+}
+
+void test_spsc_push_pop(int test_queue_size, int test_iteration, int test_element_count)
+{
+	AtomicBoundedQueue<int> queue(test_queue_size);
+
+	tbb::tick_count start = tbb::tick_count::now();
+	printf("verifying single-producer-single-consumer scenario, pushing/poping %d elements, queue size = %d, iteration count = %d...", test_element_count, test_queue_size, test_iteration);
+	for(int i=0;i<test_iteration;++i)
+	{
+		boost::thread producer(boost::bind(producer_thread_proc, &queue, test_element_count));
+		boost::thread consumer(boost::bind(consumer_thread_proc, &queue, test_element_count));
+
+		producer.join();
+		consumer.join();
+	}
+	printf("passed, time = %f ms\n", (tbb::tick_count::now() - start).seconds() * 1000.0);
+}
+
+BOOST_AUTO_TEST_CASE( AtomicBoundedQueueTestCase2_SingleProducerSingleConsumer )
+{
+	test_spsc_push_pop(64, 100, 65536);
+	test_spsc_push_pop(128, 100, 65536);
+	test_spsc_push_pop(256, 100, 65536);
+	test_spsc_push_pop(512, 100, 65536);
+	test_spsc_push_pop(1024, 100, 65536);
+}
+
+void test_mpsc_push_pop(int test_queue_size, int test_iteration, int test_element_count, int producer_count)
+{
+	AtomicBoundedQueue<int> queue(test_queue_size);
+
+	tbb::tick_count start = tbb::tick_count::now();
+	printf("verifying %d-producer-single-consumer scenario, pushing/poping %d elements, queue size = %d, iteration count = %d...", producer_count, test_element_count, test_queue_size, test_iteration);
+	for(int i=0;i<test_iteration;++i)
+	{
+		// create producer threads
+		std::vector<boost::thread*> producers;
+		for(int j=0;j<producer_count;++j)
+		{
+			producers.push_back(new boost::thread(boost::bind(producer_thread_proc, &queue, test_element_count/producer_count)));
+		}
+
+		// create consumer thread
+		boost::thread consumer(boost::bind(consumer_thread_proc, &queue, test_element_count));
+
+		for(int j=0;j<producer_count;++j)
+		{
+			producers[j]->join();
+		}
+
+		consumer.join();
+
+		// cleanup
+		for(int j=0;j<producer_count;++j)
+		{
+			delete producers[j];
+		}
+		producers.clear();
+	}
+	printf("passed, time = %f ms\n", (tbb::tick_count::now() - start).seconds() * 1000.0);
+}
+
+BOOST_AUTO_TEST_CASE( AtomicBoundedQueueTestCase3_MultipleProducerSingleConsumer )
+{
+	test_mpsc_push_pop(64, 100, 65536, 2);
+	test_mpsc_push_pop(128, 100, 65536, 2);
+	test_mpsc_push_pop(256, 100, 65536, 2);
+	test_mpsc_push_pop(512, 100, 65536, 2);
+	test_mpsc_push_pop(1024, 100, 65536, 2);
+
+	test_mpsc_push_pop(64, 100, 65536, 4);
+	test_mpsc_push_pop(128, 100, 65536, 4);
+	test_mpsc_push_pop(256, 100, 65536, 4);
+	test_mpsc_push_pop(512, 100, 65536, 4);
+	test_mpsc_push_pop(1024, 100, 65536, 4);
+}
+
+void test_mpmc_push_pop(int test_queue_size, int test_iteration, int test_element_count, int producer_count, int consumer_count)
+{
+	AtomicBoundedQueue<int> queue(test_queue_size);
+
+	tbb::tick_count start = tbb::tick_count::now();
+	printf("verifying %d-producer-%d-consumer scenario, pushing/poping %d elements, queue size = %d, iteration count = %d...", producer_count, consumer_count, test_element_count, test_queue_size, test_iteration);
+	for(int i=0;i<test_iteration;++i)
+	{
+		// create producer threads
+		std::vector<boost::thread*> producers;
+		for(int j=0;j<producer_count;++j)
+		{
+			producers.push_back(new boost::thread(boost::bind(producer_thread_proc, &queue, test_element_count/producer_count)));
+		}
+
+		// create consumer thread
+		std::vector<boost::thread*> consumers;
+		for(int j=0;j<consumer_count;++j)
+		{
+			consumers.push_back(new boost::thread(boost::bind(consumer_thread_proc, &queue, test_element_count/consumer_count)));
+		}
+
+		for(int j=0;j<producer_count;++j)
+		{
+			producers[j]->join();
+		}
+
+		for(int j=0;j<consumer_count;++j)
+		{
+			consumers[j]->join();
+		}
+
+		// cleanup
+		for(int j=0;j<producer_count;++j)
+		{
+			delete producers[j];
+		}
+		producers.clear();
+
+		for(int j=0;j<consumer_count;++j)
+		{
+			delete consumers[j];
+		}
+		consumers.clear();
+	}
+	printf("passed, time = %f ms\n", (tbb::tick_count::now() - start).seconds() * 1000.0);
+}
+
+BOOST_AUTO_TEST_CASE( AtomicBoundedQueueTestCase4_MultipleProducerMultipleConsumer )
+{
+	test_mpmc_push_pop(64, 100, 65536, 2, 2);
+	test_mpmc_push_pop(128, 100, 65536, 2, 2);
+	test_mpmc_push_pop(256, 100, 65536, 2, 2);
+	test_mpmc_push_pop(512, 100, 65536, 2, 2);
+	test_mpmc_push_pop(1024, 100, 65536, 2, 2);
+
+	test_mpmc_push_pop(64, 100, 65536, 4, 4);
+	test_mpmc_push_pop(128, 100, 65536, 4, 4);
+	test_mpmc_push_pop(256, 100, 65536, 4, 4);
+	test_mpmc_push_pop(512, 100, 65536, 4, 4);
+	test_mpmc_push_pop(1024, 100, 65536, 4, 4);
+}
+BOOST_AUTO_TEST_SUITE_END()
